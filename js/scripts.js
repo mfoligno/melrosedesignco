ade52ff29de3a590f75c6eab8e77ec7985ed033e
John MacBook Air <digitalshore@icloud.com>
2016-05-05 19:14:27 -0400

    updating scrolling to anchors and contact form

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,8 @@
-//test
-//this is a test
+(function() {
+  $('a[href*="#"]').click(function() {
+    var link = $(this).attr("href");
+    $('html, body').animate({
+      scrollTop: $(link).offset().top - 90
+    }, 400);
+  });
+})();
